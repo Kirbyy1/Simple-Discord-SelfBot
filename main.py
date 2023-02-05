b1a1f692d3bfbac7162f6371abdc756040222d57
Kirbyy <81638158+Kirbyy1@users.noreply.github.com>
2023-02-05 13:27:46 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import requests
+import uuid
+import random
+
+
+random_user_agent = random.choice(requests.get(
+    'https://gist.githubusercontent.com/pzb/b4b6f57144aea7827ae4/raw/cf847b76a142955b1410c8bcef3aabe221a63db1/user-agents.txt').text.split(
+    '\n'))
+
+
+# 401 = Bad auth token
+# 429 = Rate Limit
+
+class Discord:
+    MESSAGES = "https://discord.com/api/v9/channels/{channel_id}/messages"
+
+    def __init__(self, token: str, **kwargs):
+        self.proxy: str = kwargs.get('proxy', {})
+
+        if not isinstance(token, str):
+            raise ValueError(f'The authentication token must be a string not a {type(token)}')
+
+        user_agent: str = kwargs.get('user_agent',
+                                     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari')
+
+        self.headers = {
+            'authorization': token,
+            'content-type': 'application/json',
+            'cache-control': 'no-cache',
+            'user-agent': user_agent,
+        }
+
+    def send_message(self, channel_id: str | int, message: str | int):
+        URL: str = self.MESSAGES.format(channel_id=channel_id)
+        guid: str = str(uuid.uuid4())
+        # tts = Text to speech
+        payload = {'content': message, 'tts': False, 'nonce': guid[:15]}
+
+        return requests.post(url=URL, headers=self.headers,
+                             json=payload, proxies=self.proxy)
+
+    def read_messages(self, channel_id: str):
+        URL: str = self.MESSAGES.format(channel_id=channel_id)
+        return requests.get(url=URL, headers=self.headers, proxies=self.proxy)
+
+    def delete_message(self, channel_id: str | int, message_id: str | int):
+        URL: str = f'{self.MESSAGES.format(channel_id=channel_id)}/{message_id}'
+        return requests.delete(url=URL, headers=self.headers, proxies=self.proxy)
+
+
+discord_client = Discord(token=auth_token, user_agent=random_user_agent)
+print(discord_client.send_message(channel_id='channel_id', message='Test').text)
